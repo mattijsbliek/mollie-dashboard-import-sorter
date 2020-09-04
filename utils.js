2381776ef4044958863ab185f28c0a6cc57be83b
Mattijs Bliek <mattijs@mollie.com>
2020-09-04 11:10:32 +0200

    Small cleanup.

--- utils.js
+++ utils.js
@@ -1,14 +1,6 @@
 /*
  * Line utils 
  */
-exports.getPathFromImport = line => {
-  const [_, path] = line.match(/^import\ (?:.+)\ from\ ['"](.+)['"];$/gm);
-
-  return path;
-}
-
-exports.lineIsImport = (line) => line.startsWith('import');
-
 exports.isRelativeImport = (path) => path.startsWith('.');
 
 const rootRelativeDirectories = /^(assets|components|containers|entries|forms|helpers|hooks|intl|messages|reduxState|services|styles|types|utils)/;
